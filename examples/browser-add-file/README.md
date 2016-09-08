ad6ed445f66fd870539e32df2ad38573240c0ef4
nycoliver <oliver@oliverfisher.nyc>
2016-09-07 23:50:58 -0400

    fix browser-add-file example

--- examples/browser-add-file/README.md
+++ examples/browser-add-file/README.md
@@ -1,4 +1,4 @@
-# JS IPFS API - Example Browser - Add
+# JS IPFS API - Example Browser - Add File
 
 ## Setup
 
@@ -6,6 +6,13 @@ Install [go-ipfs](https://ipfs.io/docs/install/) and run it
 
 ```bash
 $ ipfs daemon
+```
+
+Set CORS so api can access your node [https://github.com/ipfs/ipfs/issues/75#issuecomment-131293095]((more info)).
+```bash
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST"]'
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Credentials '["true"]'
 ```
 
 then in this folder run
--- examples/browser-add-file/index.js
+++ examples/browser-add-file/index.js
@@ -3,48 +3,30 @@
 
 var ipfs = window.IpfsApi()
 
-function storeText () {
-  store(document.getElementById('source').value)
-}
-
-function storeFile () {
-  const file = document.getElementById('filePicker').files[0]
+function store () {
+  const file = document.getElementById('source').files[0]
   const reader = new FileReader()
   reader.onload = function () {
-    store(reader.result)
+    var toStore = new Buffer(reader.result);
+    ipfs.add(toStore, function (err, res) {
+      if (err || !res) {
+        return console.error('ipfs add error', err, res)
+      }
+
+      res.forEach(function (file) {
+        console.log('successfully stored', file)
+        display(file.path)
+      })
+    })
   }
   reader.readAsArrayBuffer(file)
 }
 
-// from examples/browser-add
-function store (toStore) {
-  ipfs.add(new window.buffer.Buffer(toStore), function (err, res) {
-    if (err || !res) {
-      return console.error('ipfs add error', err, res)
-    }
-
-    res.forEach(function (file) {
-      console.log('successfully stored', file)
-      display(file.path)
-    })
-  })
-}
-
 function display (hash) {
-  ipfs.cat(hash, function (err, res) {
-    if (err || !res) {
-      return console.error('ipfs cat error', err, res)
-    }
-    if (res.readable) {
-      console.error('unhandled: cat result is a pipe', res)
-    } else {
-      document.getElementById('hash').innerText = hash
-      document.getElementById('content').innerText = res
-    }
-  })
+  document.getElementById('hash').innerHTML =
+    "<a href='http://localhost:8080/ipfs/"+hash+"'>"+hash+"</a>"
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('storeText').onclick = storeText
-  document.getElementById('storeBuffer').onclick = storeFile
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('store').onclick = store
 })
